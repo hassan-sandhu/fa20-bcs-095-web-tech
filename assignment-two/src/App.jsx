4eb8ceee70f649bef4eb69167dd3120005c6610c
Hassan Khalid <hassankhalidsandhu@gmail.com>
2024-03-21 11:00:42 +0500

    Update App.jsx

--- assignment-two/src/App.jsx
+++ assignment-two/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        
       </Routes>
     </Router>
   );
