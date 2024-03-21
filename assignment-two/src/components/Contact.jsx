d7d1c4e60fd3fd70d50ceb17c7219523c172f131
Hassan Khalid <hassankhalidsandhu@gmail.com>
2024-03-21 11:00:54 +0500

    Update Contact.jsx

--- assignment-two/src/components/Contact.jsx
+++ assignment-two/src/components/Contact.jsx
@@ -1,35 +1,93 @@
 import React from "react";
 import "../styles/contact.css";
+import {useState} from "react";
+
 
 const Contact = () => {
+
+  const [errors, setErrors] = useState({}); 
+
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+
+  const changeHandler = (event) => {
+
+    const name = event.target.name;
+    const value = event.target.value;
+    
+
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newErrors = {};
+    for (const key in formData) {
+      if (formData[key].trim() === "") {
+        newErrors[key] = true;
+      }
+    }
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length === 0) {
+      alert("Form Data Submitted Successfully");
+      setFormData({
+        name:"",
+        email:"",
+        message:""
+      });
+
+    }
+  };
+
   return (
     <div className="contactus">
       <div className="contact-bg"></div>
 
-      <div class="form-container">
+      <div className="form-container">
         <h1>CONTACT US</h1>
-        <form id="contactForm">
-          <div className="name">
-            <input type="text" name="name" id="name" placeholder="Your Name" />
-            <span id="nameError" class="error"></span>
+        <form id="contactForm" onSubmit={submitHandler}>
+          <div className={errors.name ? "name error" : "name"}>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Your Name"
+              value={formData.name}
+              onChange={changeHandler}
+            />
+            {errors.name && <span className="error">Name is required</span>}
           </div>
-          <div className="email">
+          <div className={errors.email ? "email error" : "email"}>
             <input
               type="email"
               name="email"
               id="email"
               placeholder="Your Email"
+              value={formData.email}
+              onChange={changeHandler}
             />
-            <span id="emailError" class="error"></span>
+            {errors.email && <span className="error">Email is required</span>}
           </div>
-          <div className="message">
+          <div className={errors.message ? "message error" : "message"}>
             <textarea
               name="message"
               id="message"
               cols="30"
               rows="10"
+              placeholder="Your Message"
+              value={formData.message}
+              onChange={changeHandler}
             ></textarea>
-            <span id="messageError" class="error"></span>
+            {errors.message && <span className="error">Message is required</span>}
           </div>
           <input type="submit" value="Submit" />
         </form>
